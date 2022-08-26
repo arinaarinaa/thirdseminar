ea2022eae69241c68a6b3a07863c2c38b9b62177
arinach <arina.tchukurova@yandex.ru>
2022-08-26 19:05:46 +0300

    add secondExer

---
+++ secondExer/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// проверяет, является ли оно палиндромом.
+//14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Пятизначное число - ");
+int x = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(x);
+
+void Proverka(string number)
+{
+    if(number[0] == number[4] && number[1] == number[3])
+    Console.WriteLine("Палиндром");
+
+    else 
+    {
+        Console.WriteLine("не");
+    }
+}
+Proverka(number);
